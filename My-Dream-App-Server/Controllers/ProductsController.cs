af846446ee5ce2a359dd42a092bc8480b102d21c
Bresleveloper <ariel.rubi@gmail.com>
2017-10-10 17:38:18 +0300

    more items

--- My-Dream-App-Server/Controllers/ProductsController.cs
+++ My-Dream-App-Server/Controllers/ProductsController.cs
@@ -16,7 +16,17 @@ namespace My_Dream_App_Server.Controllers
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
             new Product { Id = 4, Name = "Star", Category = "Heavens", Price = 116.99M },
             new Product { Id = 5, Name = "Hero", Category = "Angular", Price = 216.99M },
-            new Product { Id = 6, Name = "Dream", Category = "Heavens", Price = 316.99M }
+            new Product { Id = 6, Name = "Dream", Category = "Heavens", Price = 316.99M },
+            new Product { Id = 7, Name = "Banana", Category = "Food", Price = 4.02M },
+            new Product { Id = 8, Name = "Chocolate", Category = "Food", Price = 5.03M },
+            new Product { Id = 9, Name = "Vanilla", Category = "Food", Price = 13.90M },
+            new Product { Id = 10, Name = "Vodka", Category = "Food", Price = 6.00M },
+            new Product { Id = 11, Name = "Coke", Category = "Food", Price = 5.99M },
+            new Product { Id = 12, Name = "Sugar", Category = "Food", Price = 5.98M },
+            new Product { Id = 13, Name = "Screwdriver", Category = "Hardware", Price = 1.99M },
+            new Product { Id = 14, Name = "Jack", Category = "Hardware", Price = 2.99M },
+            new Product { Id = 15, Name = "Doll", Category = "Toys", Price = 3.99M },
+
         };
 
         public IEnumerable<Product> GetAllProducts()
